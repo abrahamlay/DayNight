cb139421bfa558ee7c38cacfcbdac83e1ea18e60
abrahamlay <abraham_lay@yahoo.co.id>
2020-03-25 23:20:59 +0700

    Initial Commit

---
+++ DayNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DayNight
+//
+//  Created by Abraham Lay (ID) on 25/03/20.
+//  Copyright © 2020 Abraham Lay (ID). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
